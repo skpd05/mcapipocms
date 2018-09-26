691d8cc5ea0f42dd23f21237dd3e690c3ae69a85
satheeshkpd <satheeshpdk@gmail.com>
2018-09-26 19:18:42 +0530

    imported to STS

--- MC-POC-REDEMPTION-MS-2/src/main/java/com/mc/demo/app/redeem/RDConfirguation.java
+++ MC-POC-REDEMPTION-MS-2/src/main/java/com/mc/demo/app/redeem/RDConfirguation.java
@@ -13,7 +13,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * @author Wipro
+ * @author SK
  *
  */
 @Configuration
